f2bf04ab7c9082fd0b0515a0135181dc5c356e27
Allen K Chiang <akchiang@ucsd.edu>
2018-03-08 10:01:19 -0800

    im tilted

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -1,5 +1,3 @@
-'use strict';
-
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
@@ -14,12 +12,12 @@ function initializePage() {
 function onServerResponse(data){
     var labElementId = getLabElementId(data.id);
     var labElement = $(labElementId);
-    labElement.css({'background': 'yellow'});
+  	labElement.css({'background': 'yellow'});
 }
 
 
 // Function to format the CSS selector
 function getLabElementId(id){
-    var labElementId = '#lab_' + id;
+    var labElementId = '#lab-' + id;
     return labElementId;
 }
